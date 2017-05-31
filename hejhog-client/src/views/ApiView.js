22a89e28b0f21c4eb761abf09e1b6bd43e6a3981
aisleypay <paisley.singh@gmail.com>
2017-05-31 09:02:29 -0400

    can get individual sublink to show names

--- hejhog-client/src/views/ApiView.js
+++ hejhog-client/src/views/ApiView.js
@@ -3,21 +3,18 @@ class ApiView {
   static render(response) {
     var html = "<div class=\"results\">"
 
-  $.each(response, function(index, api) {
-    var individual = `<a id=${api.id} class="api-base-link" href="#"> ${api.site_name}</a>`
-    html += individual
-  })
+    $.each(response, function(index, api) {
+      var individual = `<a id=${api.id} class="api-base-link" href="#"> ${api.site_name}</a>`
+      html += individual
+    })
 
-  html += "</div>"
-  $("#existing-api-links").html(html)
-  createApiListeners()
+    html += "</div>"
+    $("#existing-api-links").html(html)
+    createApiListeners()
   }
 
   static individualApiRender(response) {
-    // fix navbar
     ApiView.buildNavBar(response)
-    // populate main-body with keys on 2nd level . arrays/objects are clickable while strings are stated
-
   }
 
   static buildNavBar(response) {
@@ -34,5 +31,9 @@ class ApiView {
     createMainPathListeners(response.base_url)
   }
 
+  static renderSubLinks() {
+    debugger
+    // html of rendered sublinks
+  }
 
 }
